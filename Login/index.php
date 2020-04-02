43329ce16a715c9a344b62d86b68cb85789f85c4
yusrilmustofa <53527287+yusrilmustofa@users.noreply.github.com>
2020-04-02 18:02:41 +0700

    Add files via upload

---
+++ Login/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <form class="" action="proses-login.php" method="post">
+      <table>
+        <tr>
+          <td> <label for="">Username</label> </td>
+          <td> <input type="text" name="username" value=""> </td>
+        </tr>
+        <tr>
+          <td> <label for="">Password</label> </td>
+          <td> <input type="password" name="password" value=""> </td>
+        </tr>
+        <tr>
+          <td></td>
+          <td>
+            <input type="submit" name="btnLogin" value="Login">
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
